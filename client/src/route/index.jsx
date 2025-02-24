a5be904c56b04391973b9b89935e412b1c43e7d5
sakshidonadkar <you@example.com>
2025-02-24 14:00:06 +0530

    subcat chnages

--- client/src/route/index.jsx
+++ client/src/route/index.jsx
@@ -13,6 +13,7 @@ import Profile from "../pages/Profile";
 import MyOrders from "../pages/MyOrders";
 import Address from "../pages/Address";
 import CategoryPage from "../pages/CategoryPage";
+import SubCategoryPage from "../pages/SubCategoryPage";
 import UploadProduct from "../pages/UploadProduct";
 import ProductAdmin from "../pages/ProductAdmin";
 import AdminPermision from "../layouts/AdminPermision";
@@ -22,131 +23,109 @@ import CartMobile from "../pages/CartMobile";
 import CheckoutPage from "../pages/CheckoutPage";
 import Success from "../pages/Success";
 import Cancel from "../pages/Cancel";
-import SubCategoryPage from "../pages/SubCategoryPage";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "",
-        element: <Home />,
-      },
-      {
-        path: "search",
-        element: <SearchPage />,
-      },
-      {
-        path: "login",
-        element: <Login />,
-      },
-      {
-        path: "register",
-        element: <Register />,
-      },
-      {
-        path: "forgot-password",
-        element: <ForgotPassword />,
-      },
-      {
-        path: "forgot-password",
-        element: <ForgotPassword />,
-      },
-      {
-        path: "verification-otp",
-        element: <OtpVerification />,
-      },
-      {
-        path: "reset-password",
-        element: <ResetPassword />,
-      },
-      {
-        path: "user",
-        element: <UserMenuMobile />,
-      },
-      {
-        path: "dashboard",
-        element: <Dashboard />,
-        children: [
-          {
-            path: "profile",
-            element: <Profile />,
-          },
-          {
-            path: "myorders",
-            element: <MyOrders />,
-          },
-          {
-            path: "address",
-            element: <Address />,
-          },
-          {
-            path: "category",
-            element: (
-              <AdminPermision>
-                <CategoryPage />
-              </AdminPermision>
-            ),
-          },
-          {
-            path: "subcategory",
-            element: (
-              <AdminPermision>
-                <SubCategoryPage />
-              </AdminPermision>
-            ),
-          },
-          {
-            path: "upload-product",
-            element: (
-              <AdminPermision>
-                <UploadProduct />
-              </AdminPermision>
-            ),
-          },
-          {
-            path: "product",
-            element: (
-              <AdminPermision>
-                {" "}
-                <ProductAdmin />
-              </AdminPermision>
-            ),
-          },
-        ],
-      },
-      {
-        path: ":category",
-        children: [
-          {
-            path: ":subCategory",
-            element: <ProductListPage />,
-          },
-        ],
-      },
-      {
-        path: "product/:product",
-        element: <ProductDisplayPage />,
-      },
-      {
-        path: "cart",
-        element: <CartMobile />,
-      },
-      {
-        path: "checkout",
-        element: <CheckoutPage/>,
-      },
-      {
-        path: "success",
-        element: <Success/>,
-      },
-      {
-        path: "cancel",
-        element: <Cancel/>,
-      },
-    ],
-  },
-]);
+    {
+        path : "/",
+        element : <App/>,
+        children : [
+            {
+                path : "",
+                element : <Home/>
+            },
+            {
+                path : "search",
+                element : <SearchPage/>
+            },
+            {
+                path : 'login',
+                element : <Login/>
+            },
+            {
+                path : "register",
+                element : <Register/>
+            },
+            {
+                path : "forgot-password",
+                element : <ForgotPassword/>
+            },
+            {
+                path : "verification-otp",
+                element : <OtpVerification/>
+            },
+            {
+                path : "reset-password",
+                element : <ResetPassword/>
+            },
+            {
+                path : "user",
+                element : <UserMenuMobile/>
+            },
+            {
+                path : "dashboard",
+                element : <Dashboard/>,
+                children : [
+                    {
+                        path : "profile",
+                        element : <Profile/>
+                    },
+                    {
+                        path : "myorders",
+                        element : <MyOrders/>
+                    },
+                    {
+                        path : "address",
+                        element : <Address/>
+                    },
+                    {
+                        path : 'category',
+                        element : <AdminPermision><CategoryPage/></AdminPermision>
+                    },
+                    {
+                        path : "subcategory",
+                        element : <AdminPermision><SubCategoryPage/></AdminPermision>
+                    },
+                    {
+                        path : 'upload-product',
+                        element : <AdminPermision><UploadProduct/></AdminPermision>
+                    },
+                    {
+                        path : 'product',
+                        element : <AdminPermision><ProductAdmin/></AdminPermision>
+                    }
+                ]
+            },
+            {
+                path : ":category",
+                children : [
+                    {
+                        path : ":subCategory",
+                        element : <ProductListPage/>
+                    }
+                ]
+            },
+            {
+                path : "product/:product",
+                element : <ProductDisplayPage/>
+            },
+            {
+                path : 'cart',
+                element : <CartMobile/>
+            },
+            {
+                path : "checkout",
+                element : <CheckoutPage/>
+            },
+            {
+                path : "success",
+                element : <Success/>
+            },
+            {
+                path : 'cancel',
+                element : <Cancel/>
+            }
+        ]
+    }
+])
 
-export default router;
+export default router
