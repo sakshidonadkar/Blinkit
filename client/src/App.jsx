d4e615dca601460ce322321287da354696e75cb3
sakshidonadkar <you@example.com>
2025-02-25 14:39:07 +0530

    app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,4 @@
 import { Outlet, useLocation } from 'react-router-dom'
-import './App.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import toast, { Toaster } from 'react-hot-toast';
